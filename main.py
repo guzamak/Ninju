ced0ddf05b5f757a6e729dd23408acfeacd15a51
guzamak <120812215+guzamak@users.noreply.github.com>
2024-04-03 05:41:21 +0700

    ui

--- main.py
+++ main.py
@@ -16,95 +16,91 @@ def show_error(text):
     CTkMessagebox(title="Error", message=text, icon="cancel")
 
 class Extra(ctk.CTkToplevel):
-    def __init__(self,app_instance):
+    def __init__(self, app_instance):
         super().__init__()
         self.title('extra window')
         self.app = app_instance
         self.geometry('750x500')
-        self.resizable(0,0)
+        self.resizable(0, 0)
         self.configure(fg_color="#333333")
         self.img_store = {}
         self.sign_dropdowns = {}
         self.sign_menus = {}
-        self.sign = ["rat","rat","rat"]
+        self.sign = ["rat", "rat", "rat"]
         self.image = {}
-        self.path_frame = self.create_frame(row=1, column=0, rowspan=1, columnspan=1)
-        self.sign_frame = self.create_frame(row=2, column=0, rowspan=1, columnspan=5)
-        self.img_frame = self.create_frame(row=3, column=0, rowspan=1, columnspan=5)
-        self.add_frame = self.create_frame(row=4, column=0, rowspan=1, columnspan=5)
+        self.path_frame = self.create_frame(row=1, column=0)
+        self.sign_frame = self.create_frame(row=2, column=0, columnspan=3)
+        self.img_frame = self.create_frame(row=3, column=0, columnspan=3)
+        self.add_frame = self.create_frame(row=4, column=0, columnspan=3)
 
-        # need restart when open 
-        for key, value in (sign_options.items()):
+        # Need to restart when opened
+        for key, value in sign_options.items():
             image = Image.open(value)
             new_width = image.width // 50
             new_height = image.height // 50
             image = image.resize((new_width, new_height))
             self.image[key] = image
-            
+
         self.create_path()
         self.create_sign_label()
         self.create_submit_button()
-        
+
     def browse_file(self):
         filepath = ctk.filedialog.askopenfilename()
         if filepath:
             self.path_entry.delete(0, tk.END)
             self.path_entry.insert(0, filepath)
 
-    def create_frame(self, row, column, rowspan=1, columnspan=1):
-        frame = ctk.CTkFrame(self,corner_radius=0,border_width=0,fg_color="#333333")
-        frame.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan, sticky="nsew")
-        for r in range(row, row + rowspan):
-            self.grid_rowconfigure(r, weight=1)
-        for c in range(column, column + columnspan):
-            self.grid_columnconfigure(c, weight=1)
+    def create_frame(self, row, column, columnspan=1):
+        frame = ctk.CTkFrame(self, corner_radius=0, border_width=0, fg_color="#333333")
+        frame.grid(row=row, column=column, columnspan=columnspan, padx=5, pady=5, sticky="nsew")
+        self.grid_columnconfigure(column, weight=1)
         return frame
-    
+
     def create_path(self):
-        path_label = ctk.CTkLabel(self.path_frame, text="File Path:",font=LABEL_FONT_STYLE)
-        path_label.grid(row=0, column=0,padx=5,pady=30)
-        self.path_entry = ctk.CTkEntry(self.path_frame,font=LABEL_FONT_STYLE)
-        self.path_entry.grid(row=0, column=1, columnspan=2, padx=5)
-        browse_button = ctk.CTkButton(self.path_frame, text="Browse", command=self.browse_file,font=LABEL_FONT_STYLE)
-        browse_button.grid(row=0, column=3, padx=5, pady=5)  
+        path_label = ctk.CTkLabel(self.path_frame, text="File Path:", font=LABEL_FONT_STYLE)
+        path_label.grid(row=0, column=0, padx=5, pady=5)
+        self.path_entry = ctk.CTkEntry(self.path_frame, font=LABEL_FONT_STYLE)
+        self.path_entry.grid(row=0, column=1, padx=5, pady=5)
+        browse_button = ctk.CTkButton(self.path_frame, text="Browse", command=self.browse_file, font=LABEL_FONT_STYLE)
+        browse_button.grid(row=0, column=2, padx=5, pady=5)
 
     def create_sign_label(self):
+        sign_label = ctk.CTkLabel(self.sign_frame, text=f"Sign :", font=LABEL_FONT_STYLE)
+        sign_label.grid(row=0, column=1, padx=50, pady=5)
         for i in range(3):
-            sign_label = ctk.CTkLabel(self.sign_frame, text=f"Sign {i+1}:",font=LABEL_FONT_STYLE)
-            sign_label.grid(row=1, column=i+1, padx=100)
 
             self.img_store[i] = ImageTk.PhotoImage(self.image["rat"])
 
-            self.sign_dropdowns[i] = tk.Menubutton(self.img_frame, image=self.img_store[i], relief=tk.RAISED,highlightthickness=0)
-            self.sign_dropdowns[i].grid(row=1, column=i+1, padx=50)
+            self.sign_dropdowns[i] = tk.Menubutton(self.img_frame, image=self.img_store[i], relief=tk.RAISED, highlightthickness=0)
+            self.sign_dropdowns[i].grid(row=0, column=i, padx=50, pady=5)
 
-        
-            self.sign_menus[i] = tk.Menu(self.sign_dropdowns[i], tearoff=False,background="#333333",borderwidth=0)
+            self.sign_menus[i] = tk.Menu(self.sign_dropdowns[i], tearoff=False, background="#333333", borderwidth=0)
             self.sign_dropdowns[i].config(menu=self.sign_menus[i])
 
-            for key, value in (self.image.items()):
+            for key, value in self.image.items():
                 self.img_store[str(i)+key] = ImageTk.PhotoImage(value)
-                self.sign_menus[i].add_command(label="", compound=tk.LEFT,image=self.img_store[str(i)+key], command=lambda value=key, var=value, index=i: self.change_option(value,index))
+                self.sign_menus[i].add_command(label="", compound=tk.LEFT, image=self.img_store[str(i)+key], command=lambda value=key, var=value, index=i: self.change_option(value, index))
 
-    def change_option(self,key,i):
+    def change_option(self, key, i):
         self.img_store[i] = ImageTk.PhotoImage(self.image[key])
         self.sign_dropdowns[i].config(image=self.img_store[i])
         self.sign[i] = key
 
     def create_submit_button(self):
-        submit = ctk.CTkButton(self.add_frame, text="Add +",font=LABEL_FONT_STYLE, command=self.add_data)
-        submit.pack()
+        submit = ctk.CTkButton(self.add_frame, text="Add +", font=LABEL_FONT_STYLE, command=self.add_data)
+        submit.grid(row=0, column=0, padx=20, pady=20)
 
     def add_data(self):
         sign = self.sign
         path = self.path_entry.get()
         if path == "" or sign[0] == sign[1] or sign[1] == sign[2]:
-           show_error("path != no ")
-           return
+            show_error("path != no ")
+            return
 
         save_data = {
-            "sign":self.sign.copy(),
-            "path":path
+            "sign": self.sign.copy(),
+            "path": path
         }
         if len(data) < 5:
             data.append(save_data)
@@ -151,6 +147,7 @@ class App:
         self.canvas = ctk.CTkCanvas(self.frame1,bg="black",borderwidth=0,highlightthickness=2, highlightbackground="#2B719E")
         self.canvas.pack(expand=True, fill="both")
         self.create_plus_button()
+        self.create_clear_current_data()
 
         data = []
         self.load_data()
@@ -181,6 +178,10 @@ class App:
         button = ctk.CTkButton(self.frame4,text="+",font=DEFAULT_FONT_STYLE,border_width=0,command=self.use_extra_window,fg_color="#333333" ,hover_color="#333333",corner_radius=0)
         button.pack(expand=True, fill="both")
 
+    def create_clear_current_data(self):
+        button = ctk.CTkButton(self.frame2,text="reset",command=self.clear_current_data)
+        button.grid(row=2, column=0, padx=10, pady=10)
+
     def use_extra_window(self):
         extra_window = Extra(self)
 
@@ -190,21 +191,28 @@ class App:
             child.destroy()
         if len(d) != 0:
             for index, item in enumerate(d):
-                frame = ctk.CTkFrame(self.scroll_frame,corner_radius=0,border_width=0,)
+                frame = ctk.CTkFrame(self.scroll_frame, corner_radius=0, border_width=0)
                 frame.pack()
+
+                canvas_width = 100
+                canvas_height = 100
+
                 for i in range(len(item['sign'])):
                     path = sign_options[item['sign'][i]]
                     img = Image.open(path)
-                    img = img.resize((200, 200))
+                    img = img.resize((canvas_width, canvas_height))
                     img = ImageTk.PhotoImage(img)
-                    canvas = ctk.CTkCanvas(frame, width=100, height=100,bg="black",borderwidth=0,highlightthickness=2, highlightbackground="#2B719E")
+                    
+                    canvas = ctk.CTkCanvas(frame, width=canvas_width, height=canvas_height, bg="black", borderwidth=0, highlightthickness=2, highlightbackground="#2B719E")
                     canvas.create_image(0, 0, anchor='nw', image=img)
-                    canvas.image = img = img
-                    canvas.grid(row=0, column=i, padx=10, pady=10)
-                label = ctk.CTkLabel(frame, text=f"Path : {item["path"]}",font=LABEL_FONT_STYLE)
-                label.grid(row=1, column=0,)
-                button = ctk.CTkButton(frame, text="delete", border_width=0, command=lambda d=d,index=index: self.del_data(index,d))
-                button.grid(row=1, column=2)
+                    canvas.image = img
+                    canvas.grid(row=0, column=i, padx=10, pady=10, sticky="nsew")
+
+                label = ctk.CTkLabel(frame, text=f"Path: {item['path'][:20]}...", font=LABEL_FONT_STYLE)
+                label.grid(row=1, column=0, columnspan=len(item['sign']), padx=10, pady=(0, 10), sticky="ew")
+
+                button = ctk.CTkButton(frame, text="Delete", border_width=0, command=lambda d=d, index=index: self.del_data(index, d))
+                button.grid(row=2, column=0, columnspan=len(item['sign']), padx=10, pady=(0, 10), sticky="ew")
 
     def del_data(self,i,d):
         d.pop(i)
@@ -214,16 +222,20 @@ class App:
     def render_current_data(self):
         for child in self.frame2.winfo_children():
             child.destroy()
+            self.create_clear_current_data()
         if len(self.current_data) != 0:
+            canvas_width = 125
+            canvas_height = 125
             for index, item in enumerate(self.current_data):
                 path = sign_options[item]
                 img = Image.open(path)
-                img = img.resize((200, 200))
+                img = img.resize((canvas_width, canvas_height))
                 img = ImageTk.PhotoImage(img)
-                canvas = ctk.CTkCanvas(self.frame2, width=100, height=100,bg="black",borderwidth=0,highlightthickness=2, highlightbackground="#2B719E")
+
+                canvas = ctk.CTkCanvas(self.frame2, width=canvas_width, height=canvas_height, bg="black", borderwidth=0, highlightthickness=2, highlightbackground="#2B719E")
                 canvas.create_image(0, 0, anchor='nw', image=img)
-                canvas.image = img = img
-                canvas.grid(row=0, column=index, padx=10, pady=10)
+                canvas.image = img
+                canvas.grid(row=1, column=index, padx=10, pady=10)
 
     def clear_current_data(self):
         self.current_data = []
@@ -240,7 +252,6 @@ class App:
         if len(class_index) != 0 and len(self.current_data) < 3:
             class_list = list(sign_options.keys())
             value = class_list[int(class_index[0])]
-            # print(class_list,int(class_index[0])-1)
             if self.sign_before != "":
                 if self.sign_before == value:
                     self.sign_count += 1
@@ -286,7 +297,6 @@ class App:
 
         self.add_current_data(class_index)
         self.window.after(1, self.update_webcam)
-        
 
     def save_data(self):
         with open("data.json", "w") as f:
